4034c1c50b4127d8f704f644ef9b281d18dbc7c2
Andy <andy.french@hotmail.co.uk>
2019-08-08 13:52:29 +0100

    Generated 'log-form' component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogFormComponent } from './components/log-form/log-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LogFormComponent
   ],
   imports: [
     BrowserModule
