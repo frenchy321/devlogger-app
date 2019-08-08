cc6dc559f981f9fdab8c02e6174e362202b4b19a
Andy <andy.french@hotmail.co.uk>
2019-08-08 14:49:33 +0100

    Generated 'logs' component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LogFormComponent } from './components/log-form/log-form.component';
+import { LogsComponent } from './components/logs/logs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LogFormComponent
+    LogFormComponent,
+    LogsComponent
   ],
   imports: [
     BrowserModule
