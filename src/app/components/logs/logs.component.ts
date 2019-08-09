fc0e1cbee55bd10d104409778540435c12b2e3a2
Andy <andy.french@hotmail.co.uk>
2019-08-09 10:52:08 +0100

    Altered text i logs HTML using string interpolation.

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
+  logs: {
+    id: string;
+    text: string;
+    date: any;
+  }[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.logs = [
+      {
+        id: '1',
+        text: 'Generated components',
+        date: new Date('01/08/2019 12:54:23')
+      },
+      {
+        id: '2',
+        text: 'Added bootstrap',
+        date: new Date('04/08/2019 14:50:20')
+      },
+      {
+        id: '3',
+        text: 'Added logs component',
+        date: new Date('06/08/2019 17:40:41')
+      }
+    ];
   }
-
 }
