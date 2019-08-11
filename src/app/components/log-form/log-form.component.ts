e57c935b6aa32fd6084ac305dfa020d507dc1bb9
Andy <andy.french@hotmail.co.uk>
2019-08-11 10:54:31 +0100

    Added  'addLog' and 'updateLog' methods.

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -28,5 +28,34 @@ export class LogFormComponent implements OnInit {
     });
   }
 
-  onSubmit() {}
+  onSubmit() {
+    // check if new log
+    if (this.isNew) {
+      // create new log
+      const newLog = {
+        id: this.generateId(),
+        text: this.text,
+        date: new Date()
+      };
+      // add log
+      this.logService.addLog(newLog);
+    } else {
+      // create log to be updated
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date()
+      };
+      // update Log
+      this.logService.updateLog(updLog);
+    }
+  }
+
+  generateId() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = (Math.random() * 16) | 0,
+        v = c == 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  }
 }
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -45,4 +45,17 @@ export class LogService {
   setFormLog(log: Log) {
     this.logSource.next(log);
   }
+
+  addLog(log: Log) {
+    this.logs.unshift(log);
+  }
+
+  updateLog(log: Log) {
+    this.logs.forEach((curr, index) => {
+      if (log.id === curr.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+    this.logs.unshift(log);
+  }
 }
