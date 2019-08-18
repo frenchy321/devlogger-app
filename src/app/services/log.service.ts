6bcec3ad6b95f53ec98c41dd7d3bbe0ff0016f84
Andy <andy.french@hotmail.co.uk>
2019-08-18 23:44:39 +0100

    Removed hard-coded logs from constructor.

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -22,23 +22,7 @@ export class LogService {
   stateClear = this.stateSource.asObservable();
 
   constructor() {
-    this.logs = [
-      {
-        id: '1',
-        text: 'Generated components',
-        date: new Date('01/08/2019 12:54:23')
-      },
-      {
-        id: '2',
-        text: 'Added bootstrap',
-        date: new Date('04/08/2019 14:50:20')
-      },
-      {
-        id: '3',
-        text: 'Added logs component',
-        date: new Date('06/08/2019 17:40:41')
-      }
-    ];
+    this.logs = [];
   }
 
   getLogs(): Observable<Log[]> {
