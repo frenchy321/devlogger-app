7885fc2b144735f5ea4fa58f647150046a25297d
Andy <andy.french@hotmail.co.uk>
2019-08-18 23:39:31 +0100

    Added 'clearState' click event to clear button on log-form.

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -9,16 +9,25 @@ import { Log } from '../../models/log';
 })
 export class LogsComponent implements OnInit {
   logs: Log[];
+  selectedLog: Log;
+  loaded: boolean = false;
 
   constructor(private logService: LogService) {}
 
   ngOnInit() {
+    this.logService.stateClear.subscribe(clear => {
+      if (clear) {
+        this.selectedLog = { id: '', text: '', date: '' };
+      }
+    });
+
     this.logService.getLogs().subscribe(logs => {
       this.logs = logs;
     });
   }
   onSelect(log: Log) {
     this.logService.setFormLog(log);
+    this.selectedLog = log;
   }
 
   onDelete(log: Log) {
